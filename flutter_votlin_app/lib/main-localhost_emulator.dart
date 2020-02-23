edc9708d9fd1979124ba2a41120443fc4762e9cc
Joaquim Puyo <Joaquim.Puyo@es.nestle.com>
2020-02-23 16:21:55 +0100

    fix for flutter unhandled exception when launching app

--- flutter_votlin_app/lib/main-localhost_emulator.dart
+++ flutter_votlin_app/lib/main-localhost_emulator.dart
@@ -3,6 +3,7 @@ import 'package:flutter_votlin_app/app/app.dart';
 import 'package:flutter_votlin_app/core/config/config.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Config.flavor = Flavor.LOCALHOST_EMULATOR;
   await MyApp.init();
   runApp(MyApp());
--- flutter_votlin_app/lib/main.dart
+++ flutter_votlin_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_votlin_app/app/app.dart';
 import 'package:flutter_votlin_app/core/config/config.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Config.flavor = Flavor.MOCK;
   await MyApp.init();
   runApp(MyApp());
